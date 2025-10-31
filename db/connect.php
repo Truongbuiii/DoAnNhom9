3cc6080368f83b2c7c0f3b5c267a747117505c1f
Truongbuiii <ductruongbui05@gmail.com>
2025-10-31 15:34:02 +0700

    up

---
+++ db/connect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Mặc định của XAMPP
+$password = ""; // XAMPP không có mật khẩu mặc định
+$dbname = "b03db";
+
+// Kết nối MySQL
+$conn =  mysqli_connect("localhost", "root","", "b03db");
+
