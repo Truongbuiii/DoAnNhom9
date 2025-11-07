f04a6eacb22cf1122f72242a4394a6ccc874a5b1
Ngoctruc2005 <ngoctruc517@gmail.com>
2025-11-07 14:52:02 +0700

    z

--- index.php
+++ index.php
@@ -25,7 +25,16 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                               <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+    <?php 
+        if (isset($_SESSION['PhanQuyen']) && $_SESSION['PhanQuyen'] == 'Admin') {
+            echo 'Admin';
+        } else {
+            echo 'Nhân viên';
+        }
+    ?>
+</span>
+
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
